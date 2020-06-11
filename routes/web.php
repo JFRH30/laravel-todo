dfb2cf4e9303e3bd4ee2af851d29614f851a61a6
Alaude30 <juliushubilla@gmail.com>
2020-06-11 15:19:02 +0800

    Secure tasks route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@showWelcome');
 
 // Login Page
-Route::get('login', 'LoginController@index');
+Route::get('login', 'LoginController@index')->name('login');
 Route::post('login', 'LoginController@authenticate');
 Route::post('logout', 'LoginController@logout');
 
@@ -26,9 +26,9 @@ Route::get('register', 'RegisterController@index');
 Route::post('register', 'RegisterController@store');
 
 // Task Page
-Route::get('task', 'TasksController@index');
-Route::post('task', 'TasksController@store');
-Route::delete('task/{id}', 'TasksController@destroy');
-Route::get('task/{id}/edit', 'TasksController@edit');
-Route::put('task/{id}/mark', 'TasksController@mark');
-Route::put('task/{id}/update', 'TasksController@update');
+Route::get('task', 'TasksController@index')->middleware('auth');
+Route::post('task', 'TasksController@store')->middleware('auth');
+Route::delete('task/{id}', 'TasksController@destroy')->middleware('auth');
+Route::get('task/{id}/edit', 'TasksController@edit')->middleware('auth');
+Route::put('task/{id}/mark', 'TasksController@mark')->middleware('auth');
+Route::put('task/{id}/update', 'TasksController@update')->middleware('auth');
